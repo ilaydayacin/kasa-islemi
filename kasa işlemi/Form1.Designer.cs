da8482ef49180efebd376b0222fd4a9275fc8551
ilayda yalçın <146716704+ilaydayacin@users.noreply.github.com>
2023-10-17 15:41:49 +0300

    Add files via upload

---
+++ kasa işlemi/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace kasa_işlemi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOde = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rb5 = new System.Windows.Forms.RadioButton();
+            this.rb4 = new System.Windows.Forms.RadioButton();
+            this.rb3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.rb2 = new System.Windows.Forms.RadioButton();
+            this.btnOdeme = new System.Windows.Forms.Button();
+            this.lblBorc = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Toplam Tutar";
+            // 
+            // txtOde
+            // 
+            this.txtOde.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtOde.Location = new System.Drawing.Point(157, 21);
+            this.txtOde.Name = "txtOde";
+            this.txtOde.Size = new System.Drawing.Size(193, 22);
+            this.txtOde.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rb5);
+            this.groupBox1.Controls.Add(this.rb4);
+            this.groupBox1.Controls.Add(this.rb3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.rb2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(17, 76);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(264, 211);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ödeme Şekli";
+            // 
+            // rb5
+            // 
+            this.rb5.AutoSize = true;
+            this.rb5.Location = new System.Drawing.Point(7, 161);
+            this.rb5.Name = "rb5";
+            this.rb5.Size = new System.Drawing.Size(177, 24);
+            this.rb5.TabIndex = 4;
+            this.rb5.TabStop = true;
+            this.rb5.Text = "5 Taksit (%10 indirim)";
+            this.rb5.UseVisualStyleBackColor = true;
+            // 
+            // rb4
+            // 
+            this.rb4.AutoSize = true;
+            this.rb4.Location = new System.Drawing.Point(6, 130);
+            this.rb4.Name = "rb4";
+            this.rb4.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.rb4.Size = new System.Drawing.Size(177, 24);
+            this.rb4.TabIndex = 3;
+            this.rb4.TabStop = true;
+            this.rb4.Text = "4 Taksit (%10 indirim)";
+            this.rb4.UseVisualStyleBackColor = true;
+            // 
+            // rb3
+            // 
+            this.rb3.AutoSize = true;
+            this.rb3.Location = new System.Drawing.Point(6, 100);
+            this.rb3.Name = "rb3";
+            this.rb3.Size = new System.Drawing.Size(168, 24);
+            this.rb3.TabIndex = 2;
+            this.rb3.TabStop = true;
+            this.rb3.Text = "3 Taksit (%5 indirim)";
+            this.rb3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 70);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(168, 24);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "2 Taksit (%5 indirim)";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // rb2
+            // 
+            this.rb2.AutoSize = true;
+            this.rb2.Location = new System.Drawing.Point(6, 39);
+            this.rb2.Name = "rb2";
+            this.rb2.Size = new System.Drawing.Size(98, 24);
+            this.rb2.TabIndex = 0;
+            this.rb2.TabStop = true;
+            this.rb2.Text = "Tek çekim";
+            this.rb2.UseVisualStyleBackColor = true;
+            // 
+            // btnOdeme
+            // 
+            this.btnOdeme.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnOdeme.Location = new System.Drawing.Point(17, 304);
+            this.btnOdeme.Name = "btnOdeme";
+            this.btnOdeme.Size = new System.Drawing.Size(264, 93);
+            this.btnOdeme.TabIndex = 3;
+            this.btnOdeme.Text = "Ödeme yap";
+            this.btnOdeme.UseVisualStyleBackColor = true;
+            this.btnOdeme.Click += new System.EventHandler(this.btnOdeme_Click);
+            // 
+            // lblBorc
+            // 
+            this.lblBorc.AutoSize = true;
+            this.lblBorc.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBorc.Location = new System.Drawing.Point(367, 176);
+            this.lblBorc.Name = "lblBorc";
+            this.lblBorc.Size = new System.Drawing.Size(115, 25);
+            this.lblBorc.TabIndex = 4;
+            this.lblBorc.Text = "Borcunuz: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblBorc);
+            this.Controls.Add(this.btnOdeme);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtOde);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtOde;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rb5;
+        private System.Windows.Forms.RadioButton rb4;
+        private System.Windows.Forms.RadioButton rb3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton rb2;
+        private System.Windows.Forms.Button btnOdeme;
+        private System.Windows.Forms.Label lblBorc;
+    }
+}
+
---
+++ kasa işlemi/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kasa_işlemi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOdeme_Click(object sender, EventArgs e)
+        {
+            double tutar=Convert.ToDouble(txtOde.Text);
+
+            if (rb2.Checked==true || rb3.Checked == true)
+            {
+                tutar = tutar + (tutar * 5)/tutar;
+            }
+            if (rb4.Checked==true || rb5.Checked == true)
+            {
+                tutar = tutar + (tutar * 10)/tutar;
+            }
+            lblBorc.Text=("Borcunuz: " + tutar);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
